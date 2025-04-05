aee96d7d31b66a12be0a8d0ff790b9cbe5bb9cf3
fickyou <kritzanis0890@gmail.com>
2025-04-05 14:43:16 +0700

    sprint 5

--- WeConnect/routes/web.php
+++ WeConnect/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',
+[HomeController::class, 'index']);
+
+Route::resource('/users', UserController::class);
