c679840e698fc3a557182904dff3fd06a3b30317
zomdoi <naphat2706@gmail.com>
2025-04-05 09:44:47 +0700

    map + from v.1

--- WeConnect/routes/web.php
+++ WeConnect/routes/web.php
@@ -8,11 +8,20 @@ use Illuminate\Support\Facades\Route;
 //     return view('login');
 // });
 
+Route::get('/', [LoginController::class, 'index']);
 Route::get('/login', [LoginController::class, 'index']);
 Route::post('/login', [LoginController::class, 'login']);
 
 Route::get('/adduser', [UserController::class, 'index']);
 Route::post('/adduser', [UserController::class, 'addUser']);
+
+Route::get('/problem', function() {
+    return view('problem');
+});
+
+Route::get('/dashboard', function() {
+    return view('dashboard');
+});
 
 Route::get('/welcome', function () {
     return view('welcome');
