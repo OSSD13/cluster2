a3ebbd66fbdc8e070bda69434e91434083e85e31
zomdoi <naphat2706@gmail.com>
2025-04-06 11:10:51 +0700

    Revert "fix bug from tata"
    This reverts commit 800f6f27f630be0f2a7e1c381db99f2fb7a335b9.

--- WeConnect/resources/views/layouts/adminmenu.blade.php
+++ WeConnect/resources/views/layouts/adminmenu.blade.php
@@ -9,6 +9,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Outfit:wght@100..900&display=swap" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
 <style>
+</head>
     body{
         font-family: 'Kanit' , sans-serif;
     }
@@ -41,7 +42,6 @@
         overflow-y: auto;
     }
 </style>
-</head>
 
 <body >
     <header class="bg-orange-400 p-4 flex items-center z-50 relative">
