0ae3b62df9b8cf9bfd089c56cac621a52107d012
Lincon001 <audomsak.kofak@gmail.com>
2025-04-09 13:41:16 +0700

    fix(Route): แก้ปัญหาที่เกิดจากการ merge

--- WeConnect/routes/web.php
+++ WeConnect/routes/web.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProblemController;
 use App\Http\Controllers\TagController;
 
-
 use Illuminate\Support\Facades\Route;
 
 
@@ -14,17 +13,18 @@ use Illuminate\Support\Facades\Route;
 //     return view('login');
 // });
 
-Route::get('/', [LoginController::class, 'index']);
-Route::get('/login', [LoginController::class, 'index']);
-Route::post('/login', [LoginController::class, 'login']);
+Route::get('/', [LoginController::class, 'index'])->name('login');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 Route::get('/usermanage', [UserController::class, 'index']);
 Route::get('/adduser', [UserController::class, 'viewAddUser']);
 Route::post('/adduser', [UserController::class, 'addUser']);
 
 // Route::get('/', [HomeController::class, 'index']); // เส้นนี้สำคัญสุด
-Route::get('/home', [ProblemController::class, 'index'])->name('home');
+Route::get('/home', [ProblemController::class, 'index'])->name('userhome');
 Route::post('/home', [ProblemController::class, 'home']);
+Route::get('/home/search', [ProblemController::class, 'search'])->name('home.search');
 
 Route::get('/problemdetail', function () {
     return view('user.problem_detail');
@@ -41,12 +41,20 @@ Route::get('/editproblem',function (){
     return view('user.edit_problem');
 });
 
+Route::get('/addmap',function (){
+    return view('user.add_map');
+});
+
 Route::get('/address',function (){
     return view('user.add_address');
 });
 
 Route::get('/testproblem',function (){
     return view('test_problem');
+});
+
+Route::get('/testaddproblem',function (){
+    return view('test_addproblem');
 });
 
 Route::get('/dashboard',function (){
@@ -57,13 +65,29 @@ Route::get('/editaddress',function (){
     return view('user.edit_address');
 });
 
-Route::get('/maps',function(){
-    return view('user.open_map');
-});
+Route::get('/maps', [ProblemController::class, 'showMap'])->name('user.open_map.view');
 
-Route::get('/edituser',function(){
-    return view('admin.edit_user');
-});
+
+Route::post('/addimage', [ProblemController::class, 'addimage'])->name('addimage');
+Route::post('/showimage', [ProblemController::class, 'showimage'])->name('showimage');
+
+// Route::get('/edituser/{id}', [UserController::class, 'viewEditUser']);
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+// Route::get('/edituser/{id}', [UserController::class, 'viewEditUser'])->name('admin.user_edit');
+
+Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+
+Route::delete('/delete/{id}', [UserController::class, 'delete'])->name('user.delete');
+
+Route::get('/users', [UserController::class, 'search'])->name('admin.manage_user');
+
+Route::get('/edituser/{id}', [UserController::class, 'viewEditUser'])->name('admin.edit_user');
+
+Route::put('/updateuser/{id}', [UserController::class, 'updateUser'])->name('user.update');
+
+
 Route::post('/tags',
 [TagController::class, 'store'])->name('tags.store');
 Route::get('/tags/fetch',
@@ -85,4 +109,5 @@ Route::get('/admindataAnalytics',function (){
     return view('admin.admin_dataAnalytics');
 });
 
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
