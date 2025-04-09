13a83a76853014a4a70582e6a9649e638f88a07a
Namtan26 <164167951+Namtan26@users.noreply.github.com>
2025-04-09 09:56:24 +0700

    Update web.php

--- WeConnect/routes/web.php
+++ WeConnect/routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
-
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProblemController;
 
 use Illuminate\Support\Facades\Route;
-
 
 // Route::get('/login', function () {
 //     return view('login');
